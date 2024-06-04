68fb16ced1ed3796796feb8b87c7e63689b9ce48
wisen <wisen1999@gmail.com>
2024-06-04 15:28:00 +0800

    feat: 增加切换翻译源命令

--- src/core/set-config.ts
+++ src/core/set-config.ts
@@ -1,10 +1,11 @@
 /**
  * 用于快捷更新用户配置
  */
+import { TranslateAPI } from "../config/translate-api";
 import * as vscode from "vscode";
 
-// 配置 cozeAI 的 botId 和 token
 export function registerUpdateConfigCommand() {
+  // 配置 cozeAI 的 botId 和 token
   vscode.commands.registerCommand("Wisen-translate.updateCozeAI", async () => {
     const botId = await vscode.window.showInputBox({
       placeHolder: "请输入 coze 的 botId",
@@ -16,5 +17,15 @@ export function registerUpdateConfigCommand() {
     const configuration = vscode.workspace.getConfiguration();
     configuration.update("WisenTranslate.cozeBotId", botId, true);
     configuration.update("WisenTranslate.cozeToken", token, true);
+  });
+
+  // 修改翻译源
+  vscode.commands.registerCommand("Wisen-translate.switchOrigin", async () => {
+    const origin = await vscode.window.showQuickPick(Object.keys(TranslateAPI)) as keyof typeof TranslateAPI;
+
+    
+    //  更新到配置文件
+    const configuration = vscode.workspace.getConfiguration();
+    configuration.update("WisenTranslate.origin", TranslateAPI[origin], true);
   });
 }
